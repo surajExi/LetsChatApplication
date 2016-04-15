b6b3ba80e58f4fc3bdbed7645fdffc66fbe532f9
surajExi <suraj.0241@gmail.com>
2016-04-15 23:22:36 +0530

    Check In
    Chat App Check In

---
+++ LetsChatApplication/Hubs/LetsChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace LetsChatApplication.Hubs
+{
+    public class LetsChatHub : Hub
+    {
+        public void Send(string name, string message,string connId)
+        {
+            Clients.Client(connId).addNewMessageToPage(name, message);
+        }
+    }
+}
